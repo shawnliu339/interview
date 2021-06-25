a17b36cb2cf02337493f3cf7b356049748ee7db4
siyuan.liu <sranliu1993@gmail.com>
2021-06-25 11:17:34 +0900

    Finished heap sort study.

--- code/HeapSort.kt
+++ code/HeapSort.kt
@@ -3,23 +3,42 @@ val parent = (i - 1) / 2
 val left = 2 * i + 1
 val right = 2 * i + 2
 
-fun adjust(array: Array<Int>, leftOrRightIndex: Int) {
-    if (leftOrRightIndex < 0) {
+fun adjust(array: Array<Int>, length: Int, index: Int) {
+    if (index >= length) {
         return
     }
-    val parentIndex = (leftOrRightIndex - 1) / 2
-    val leftIndex = 2 * parentIndex + 1
-    val rightIndex = 2 * parentIndex + 2
+    val leftIndex = 2 * index + 1
+    val rightIndex = 2 * index + 2
 
-    if (leftIndex < array.size && array[parentIndex] < array[leftIndex]) {
-        swap(array, parentIndex, leftIndex)
+    var max = index
+    if (leftIndex < length && array[max] < array[leftIndex]) {
+        max = leftIndex
     }
 
-    if (rightIndex < array.size && array[parentIndex] < array[rightIndex]) {
-        swap(array, parentIndex, rightIndex)
+    if (rightIndex < length && array[max] < array[rightIndex]) {
+        max = rightIndex
     }
 
-    adjust(array, leftOrRightIndex - 2)
+    if (max != index) {
+        swap(array, index, max)
+        adjust(array, length, max)
+    }
+}
+
+fun buildMaxHeap(array: Array<Int>) {
+    val parent = (array.lastIndex - 1) / 2
+    for (i in parent downTo 0) {
+        adjust(array, array.size, i)
+    }
+}
+
+fun heapSort(array: Array<Int>) {
+    buildMaxHeap(array)
+
+    for (i in array.lastIndex downTo 1) {
+        swap(array, 0, i)
+        adjust(array, i, 0)
+    }
 }
 
 fun swap(array: Array<Int>, left: Int, right: Int) {
@@ -27,3 +46,7 @@ fun swap(array: Array<Int>, left: Int, right: Int) {
     array[left] = array[right]
     array[right] = tmp
 }
+
+val array = arrayOf(2, 5, 3, 1, 10, 4)
+heapSort(array)
+array.toList()
