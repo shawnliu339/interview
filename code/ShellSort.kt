3f71c7e30f38b9892194adb69dc25df4b4bf69c9
siyuan.liu <sranliu1993@gmail.com>
2021-08-14 21:04:01 +0900

    Polihed shell sort

--- code/ShellSort.kt
+++ code/ShellSort.kt
@@ -1,3 +1,8 @@
+/**
+ * 第一种写法，与网上多数给出的动图不一致。
+ * 该算法先比较每一组中的第一组数，然后，比较每一组中的第二组数，直至完成排序。
+ * 具体解释可参见自己整理的数据结构预算法-排序.md的文章。
+ */
 fun shellSort(array: Array<Int>) {
     var gap = array.size / 2
     while (gap > 0) {
@@ -13,6 +18,28 @@ fun shellSort(array: Array<Int>) {
             }
         }
         gap /= 2
+    }
+}
+
+/**
+ * 第二种写法，与该网站的动图的执行顺序完全一致。
+ * https://www.cnblogs.com/onepixel/p/7674659.html
+ * 先将第一组中的所有数比较完成，然后再比较第二组数中的所有数，以此类推。
+ */
+fun shellSort2(array: Array<Int>) {
+    val random = 2
+    for (gap in random downTo 1) {
+        for (set in 0..gap) {
+            for (i in (set + gap + 1)..array.lastIndex step (gap + 1)) {
+                var j = i
+                val pivot = array[j]
+                while (j - gap - 1 >= set && pivot < array[j - gap - 1]) {
+                    array[j] = array[j - gap - 1]
+                    j = j - gap - 1
+                }
+                array[j] = pivot
+            }
+        }
     }
 }
 
